8f81f6e8d0e2269e7f9aac5a0d9128cbd18a17bf
KevinZav <kevinzavala.42@hotmail.com>
2023-05-11 22:41:52 -0600

    chore: add colors and typography constants

---
+++ src/app/shared/constants/typography.js
@@ -0,0 +1,17 @@
+export const typographyTags = [
+  "h1",
+  "h2",
+  "h3",
+  "h4",
+  "subtitle-1",
+  "subtitle-2",
+  "subtitle-3",
+  "subtitle-4",
+  "body-1",
+  "body-2",
+  "body-3",
+  "label",
+  "metadata"
+];
+
+export const typographyLights = [ '700', '500', '400', '300' ];
