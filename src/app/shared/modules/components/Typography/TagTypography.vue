b9bbbb05ef60a3ff77f6db2dbab9a75b2e10f8b8
KevinZav <kevinzavala.42@hotmail.com>
2023-05-11 22:42:23 -0600

    feat: add typography component

---
+++ src/app/shared/modules/components/Typography/TagTypography.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1 v-if="variant === 'h1'"><slot></slot></h1>
+    <h2 v-else-if="variant === 'h2'"><slot></slot></h2>
+    <h3 v-else-if="variant === 'h3'"><slot></slot></h3>
+    <h4 v-else-if="variant === 'h4'"><slot></slot></h4>
+    <h5 v-else-if="variant.startsWith('subtitle')" :class="variant"><slot></slot></h5>
+    <p v-else-if="variant.startsWith('body')" :class="variant"><slot></slot></p>
+    <small v-else :class="variant"><slot></slot></small>
+</template>
+
+<script>
+export default {
+  name: 'TagTypography',
+  props: {
+    variant: {
+      type: String,
+      required: true,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
