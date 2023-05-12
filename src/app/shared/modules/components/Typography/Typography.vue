27fd8ddd95beb2a7f2be2fa00614666cc1bf885a
KevinZav <kevinzavala.42@hotmail.com>
2023-05-11 22:42:49 -0600

    feat: add typography main component

---
+++ src/app/shared/modules/components/Typography/Typography.vue
@@ -0,0 +1,74 @@
+<template>
+    <tag-typography v-if="!!text" :class="assignClass" :variant="variant">{{ text }}</tag-typography>
+    <tag-typography v-else :class="assignClass" :variant="variant"><slot></slot></tag-typography>
+</template>
+
+<script>
+import { typographyLights, typographyTags, colors } from '@/app/shared/constants'
+import TagTypography from './TagTypography.vue'
+
+export default {
+  name: 'TypographyComponent',
+  components: {
+    TagTypography,
+  },
+  props: {
+    text: {
+      type: String,
+    },
+    variant: {
+      type: String,
+      default: 'h1',
+      validator (value) {
+        return typographyTags.includes(value)
+      }
+    },
+    fontBold: {
+      type: Boolean,
+      default: false
+    },
+    light: {
+      type: String,
+      default: '700',
+      validator (value) {
+        return typographyLights.includes(value)
+      }
+    },
+    color: {
+      type: String,
+      default: 'black',
+      validator (value) {
+        return colors.includes(value)
+      }
+    },
+    extraStyles: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    assignClass () {
+      const { color, light, extraStyles, fontBold} = this
+
+      return `${extraStyles} text-${color}-${light} ${fontBold ? 'bold' : ''}`
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@use '@/assets/styles/global/colors' as *;
+@use '@/assets/styles/global/typography' as *;
+
+@each $color in $colors {
+  @each $light in $lights {
+    .text-#{$color}-#{$light} {
+      color: map-get($map: map-get($map: $colors-map, $key: $color), $key: $light);
+    }
+  }
+}
+
+.bold {
+  font-weight: bold;
+}
+</style>
