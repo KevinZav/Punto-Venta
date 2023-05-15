6ccda7b7369bb753592b5f33a3a5ba5481aaefd8
KevinZav <kevin.zavala@umvel.com>
2023-05-15 02:25:46 -0500

    feat: add icon component

---
+++ src/app/shared/modules/components/Icon/Icon.vue
@@ -0,0 +1,39 @@
+<template>
+  <component :is="icon" :class="assignClass"></component>
+</template>
+<script>
+import { heroIcons } from "@/app/shared/utilities/icons";
+
+export default {
+  name: "IconComponent",
+  components: {
+    ...heroIcons,
+  },
+  props: {
+    icon: {
+      required: true,
+      default: "MicrophoneIcon",
+    },
+    size: {
+      type: String,
+      default: "4",
+    },
+    color: {
+      type: String,
+    },
+    extraClass: {
+      type: String,
+    },
+  },
+  computed: {
+    assignClass() {
+      const { size, color, extraClass } = this;
+
+      return `w-[${size}] w-${size} aspect-square ${
+        color ? `text-${color}` : ""
+      } ${extraClass}`;
+    },
+  },
+};
+</script>
+<style></style>
