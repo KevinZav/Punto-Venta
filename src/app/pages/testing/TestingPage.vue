4e2bd4d364747b271055924ceaaae01f77485dc9
KevinZav <kevinzavala.42@hotmail.com>
2023-05-16 23:10:11 -0600

    chore: implement sidebar and navbar in testing

--- src/app/pages/testing/TestingPage.vue
+++ src/app/pages/testing/TestingPage.vue
@@ -1,22 +1,28 @@
 <template>
-  <div class="container">
-    <typography :variant="'h1'" :color="'red-700'">
-      Hello, <span class="text-underline">there</span>. Welcome to Testing Page
-    </typography>
-    <typography
-      :variant="'body-2'"
-      :color="'blue-400'"
-      :text="'You can do some tests here about your components'"></typography>
-      <typography
-        :variant="'body-1'"
-        :color="'red-100'"
-        :text="'Are you ready?'"
-        :fontBold="true"></typography>
-    <hr class="bg-red-400">
-    <div class="buttons">
-      <h3>Buttons</h3>
-      <div class="flex gap-3 m-5">
-        <action-button @onClick="onClickButton" :text="'Default'" :variant="'primary-outline pill'"/>
+  <navbar></navbar>
+  <side-bar></side-bar>
+  <div class="p-4 sm:ml-64">
+    <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+      <div class="grid grid-cols-3 gap-4 mb-4">
+        <div class="container">
+          <typography :variant="'h1'" :color="'red-700'">
+            Hello, <span class="text-underline">there</span>. Welcome to Testing Page
+          </typography>
+          <typography :variant="'body-2'" :color="'blue-400'" :text="'You can do some tests here about your components'">
+          </typography>
+          <typography :variant="'body-1'" :color="'red-100'" :text="'Are you ready?'" :fontBold="true"></typography>
+          <hr class="bg-red-400">
+          <div class="buttons">
+            <h3>Buttons</h3>
+            <div class="flex gap-3 m-5">
+              <action-button
+                @onClick="onClickButton"
+                :variant="'red-outline icon'"
+                :icon="'MoonIcon'"
+                :iconPosition="'right'"/>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -25,14 +31,19 @@
 <script>
 import Typography from '@/app/shared/modules/components/Typography/Typography.vue'
 import ActionButton from '@/app/shared/modules/components/ActionButton/ActionButton.vue'
+import Navbar from '@/app/shared/modules/components/Navigation/Navbar.vue'
+import SideBar from '@/app/shared/modules/components/Navigation/SideBar.vue'
+
 export default {
   name: 'TestingPage',
   components: {
     Typography,
-    ActionButton
+    ActionButton,
+    Navbar,
+    SideBar
   },
   methods: {
-    onClickButton () {
+    onClickButton() {
       alert(`It's Britney, bitch!`)
     }
   }
@@ -41,6 +52,7 @@ export default {
 
 <style lang="scss" scoped>
 @use '@/assets/styles/global/colors' as *;
+
 .container {
   hr {
     height: 1px;
