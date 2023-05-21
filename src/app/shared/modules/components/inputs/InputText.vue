a9823a4c88d6f9ad244c6fc813e3314ccafdf294
KevinZav <kevin.zavala@umvel.com>
2023-05-21 12:19:36 -0500

    feat: add input text component

---
+++ src/app/shared/modules/components/inputs/InputText.vue
@@ -0,0 +1,96 @@
+<template>
+  <div :class="`relative input-text-floating ${status || variant}`">
+    <input type="text" id="floating_outlined" class="peer" :placeholder="placeholder" v-model="value" />
+    <label for="floating_outlined"
+      class="peer-focus:activate peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2">
+      {{ labelText }}
+    </label>
+  </div>
+</template>
+
+<script>
+import { inputStatus }  from '@/app/shared/constants/input'
+
+export default {
+  name: 'InputText',
+  props: {
+    modelValue: {
+      type: String,
+      default: '',
+    },
+    labelText: {
+      type: String,
+      required: true
+    },
+    placeholder: {
+      type: String
+    },
+    variant: {
+      type: String,
+      default: 'primary',
+      validator(value) {
+        return ['primary', 'purple', 'red', 'green', 'yellow'].includes(value)
+      }
+    },
+    validators: {
+      type: Array
+    },
+    setVariantByStatus: {
+      type: Boolean,
+      default: false
+    }
+  },
+  emits: ['update:modelValue'],
+  data () {
+    return {
+      status: null
+    }
+  },
+  methods: {
+    evalValidator (validator, value) {
+      if (typeof validator === 'function') {
+        return validator(value)
+      } else {
+        return validator.every(item => item(value))
+      }
+    },
+    async evalValidatorAsync (validator, value) {
+      if (typeof validator === 'function') {
+        return await validator(value)
+      } else {
+        const allValidators = await Promise.all(validator.map(item => item(value)))
+
+        return allValidators.every(item => item)
+      }
+    }
+  },
+  computed: {
+    value: {
+      get() {
+        return this.modelValue
+      },
+      set(value) {
+        this.$emit('update:modelValue', value)
+      }
+    }
+  },
+  watch: {
+    async modelValue(value) {
+
+      if (!this.setVariantByStatus || !this.validators) return
+
+      const { SUCCESS, ERROR, PENDING } = inputStatus
+
+      this.status = PENDING
+
+      const [validators, asyncValidators] = this.validators
+      const evalValidators = this.evalValidator(validators, value)
+      const evalAsyncValidators = await this.evalValidatorAsync(asyncValidators, value)
+      
+      this.status = evalValidators && evalAsyncValidators ? SUCCESS : ERROR
+    }
+  }
+}
+</script>
+
+<style></style>
